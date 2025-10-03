70389c994f848b3a2b87f5a40cc2149c68cb8492
vijaistark <vijayaraja101125@gmail.com>
2025-10-03 23:22:01 +0530

    Fix vite.config.js for GitHub Pages and add deploy script

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/resume-pdf/', // use '/<repo-name>/' exactly
   plugins: [react()],
-  base: '/resume-pdf/',   // 👈 repo name (important for GitHub Pages)
-})
+});
